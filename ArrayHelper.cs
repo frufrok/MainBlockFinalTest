8be549c615b71a3c6ffaf4df2992c39c652bd2a7
frufrok <frufrok@gmail.com>
2023-10-17 20:40:06 +0300

    Program.cs and ArrayHelper.cs are updated.

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -1,4 +1,54 @@
- public static class ArrayHelper
+public static class ArrayHelper
 {
-
+    /// <summary>
+    /// Возвращает массив строк, образованный выбором из исходного массива строк только тех, что короче заданного количества символов.
+    /// </summary>
+    /// <param name="array">Исходный массив строк.</param>
+    /// <param name="maxChars">Максимальное количество символов в строке.</param>
+    /// <returns></returns>
+    public static string[] SelectStringsShorterThan(string[] array, int maxChars = 3)
+    {
+        int count = array.Length;
+        string[] result = new string[count];
+        int k = 0;
+        for (int i = 0; i < count; i++)
+        {
+            if (array[i].Length <= maxChars)
+            {
+                result[k] = array[i];
+                k++;
+            }
+        }
+        Array.Resize(ref result, k);
+        return result;
+    }
+    /// <summary>
+    /// Возвращает строку, образованную объединением элементов массива строк, разделенных набором символов. Результат заключен в квадратные скобки.
+    /// </summary>
+    /// <param name="array">Массив строк для объединения.</param>
+    /// <param name="separator">Набор символов для разделения элементов массива в результирующей строке.</param>
+    /// <returns></returns>
+    public static string JoinString(this string[] array, string separator = ", ")
+    {
+        return $"[{String.Join(separator, array)}]";
+    }
+    /// <summary>
+    /// Записывает массив строк в консоль.
+    /// </summary>
+    /// <param name="array">Массив строк для записи.</param>
+    /// <param name="message">Сообщение перед выводом массива.</param>
+    public static void WriteStringArray(string[] array, string message = "")
+    {
+        if (message != "") Console.WriteLine(message);
+        Console.WriteLine(array.JoinString());
+    }
+    /// <summary>
+    /// Записывает массив строк в консоль.
+    /// </summary>
+    /// <param name="array">Массив строк для записи.</param>
+    /// <param name="message">Сообщение перед выводом массива.</param>
+    public static void WriteStringArrayExt(this string[] array, string message = "")
+    {
+        WriteStringArray(array, message);
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿string[] sample1 = new string[] { "Hello", "2", "world", ":-)" };
+sample1.WriteStringArrayExt("Пример 1. Входной массив:");
+ArrayHelper.WriteStringArray(ArrayHelper.SelectStringsShorterThan(sample1), "Выходной массив:");
+Console.WriteLine();
+
+string[] sample2 = new string[] {"1234", "1567", "-2", "computer science"};
+sample2.WriteStringArrayExt("Пример 2. Входной массив:");
+ArrayHelper.WriteStringArray(ArrayHelper.SelectStringsShorterThan(sample2), "Выходной массив:");
+Console.WriteLine();
+
+string[] sample3 = new string[] { "Russia", "Denmark", "Kazan" };
+sample3.WriteStringArrayExt("Пример 3. Входной массив:");
+ArrayHelper.WriteStringArray(ArrayHelper.SelectStringsShorterThan(sample3), "Выходной массив:");
+Console.WriteLine();
+
+Console.WriteLine("Вы можете проверить, как работает эта программа. Введите количество элементов для заполнения массива:");
+int n;
+try
+{
+    n = Convert.ToInt32(Console.ReadLine());   
+}
+catch
+{
+    Console.WriteLine("Ошибка ввода. Длина массива принята равной 5.");
+    n = 5;
+}
+
+if (n<0)
+{
+    Console.WriteLine($"Вы ввели число {n} меньше нуля. Будет использовано значение 5.");
+    n = 5;
+}
+
+Console.WriteLine("Введите последовательно строки для заполнения массива. Для ввода нажмите Enter.");
+string[] sample4 = new string[n];
+for (int i = 0; i < n; i++)
+{
+    sample4[i] = Console.ReadLine()??"";
+}
+sample4.WriteStringArrayExt("Пример 4. Входной массив:");
+ArrayHelper.WriteStringArray(ArrayHelper.SelectStringsShorterThan(sample4), "Выходной массив:");
+Console.WriteLine();
+
+Console.WriteLine("Нажмите любую клавишу для завершения программы.");
+Console.ReadKey();
